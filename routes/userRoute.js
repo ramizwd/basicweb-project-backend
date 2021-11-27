7304ba04be9b449886414947b4abf5bdf20e8e0b
R <63073421+ramizwd@users.noreply.github.com>
2021-11-27 17:35:39 +0200

    user route and db query for getting all the users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+const { user_get_all } = require('../controllers/userController');
+
+router.route('/').get(user_get_all);
+
+module.exports = router;
