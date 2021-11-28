c4dffd38713b39284ffd57641441b76c4e4d7148
R <63073421+ramizwd@users.noreply.github.com>
2021-11-28 15:00:11 +0200

    added get user by id route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { getAllUsers } = require('../models/userModel');
+const { getAllUsers, getUser } = require('../models/userModel');
 
 // Get all users from the database and send them in a JSON formatted response.
 const user_get_all = async (req, res) => {
@@ -9,7 +9,15 @@ const user_get_all = async (req, res) => {
     res.json(users);
 };
 
+// Get user by id from DB
+const user_get = async (req, res) => {
+    const user = await getUser(req.params.userId);
+    console.log('get user by id', user);
+    res.json(user);
+};
+
 // Export functions
 module.exports = {
     user_get_all,
+    user_get,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-const { user_get_all } = require('../controllers/userController');
+const { user_get_all, user_get } = require('../controllers/userController');
 
-router.route('/').get(user_get_all);
+router.route('/').get(user_get_all); // Get request to user_get_all using express routing
+
+router.route('/:userId').get(user_get); // Get user by id
 
 module.exports = router;
