70b915515dc7986ea66dd900407866255a326291
R <63073421+ramizwd@users.noreply.github.com>
2021-11-28 21:00:28 +0200

    post GET route

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const { getAllPosts } = require('../models/postModel');
+const { httpError } = require('../utils/errors');
+
+const post_get_all = async (req, res, next) => {
+    const posts = await getAllPosts(next);
+    console.log('get all posts', posts);
+
+    if (posts.length > 0) {
+        res.json(posts);
+        return;
+    }
+
+    const err = httpError('posts not found', 404);
+    next(err);
+};
+
+module.exports = {
+    post_get_all,
+};
