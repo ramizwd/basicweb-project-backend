bc4e8edf431ff9a5edceab0f1fc98620377b6096
R <63073421+ramizwd@users.noreply.github.com>
2021-11-29 18:55:14 +0200

    get post by id

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: './uploads/' });
-const { post_get_all } = require('../controllers/postController');
+const { post_get_all, post_get } = require('../controllers/postController');
 const router = express.Router();
 
-router.route('/').get(post_get_all);
+router.route('/').get(post_get_all); // Get request for getting all posts
+
+router.route('/:postId').get(post_get); // Get post by id request
 
 module.exports = router;
