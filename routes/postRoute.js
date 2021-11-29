17854edfeceb0d0fd3a58cb30de93bf2f09052af
R <63073421+ramizwd@users.noreply.github.com>
2021-11-29 20:11:59 +0200

    updated sql queries and added insert new post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -3,10 +3,19 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: './uploads/' });
-const { post_get_all, post_get } = require('../controllers/postController');
+const {
+    post_get_all,
+    post_get,
+    post_insert,
+} = require('../controllers/postController');
 const router = express.Router();
 
-router.route('/').get(post_get_all); // Get request for getting all posts
+router
+    .route('/')
+    // Get request for getting all posts
+    .get(post_get_all)
+    // Post request for posting new post
+    .post(post_insert);
 
 router.route('/:postId').get(post_get); // Get post by id request
 
