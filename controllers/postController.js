40a9551c1019ac17231637b397d7044c09f49620
R <ramz9_97@hotmail.com>
2021-11-29 18:55:14 +0200

    get post by id

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const { getAllPosts } = require('../models/postModel');
+const { getAllPosts, getPost } = require('../models/postModel');
 const { httpError } = require('../utils/errors');
 
+// Return a JSON array of the Posts if there is any otherwise send an error message
+// with status code to the client
 const post_get_all = async (req, res, next) => {
     const posts = await getAllPosts(next);
     console.log('get all posts', posts);
@@ -16,6 +18,23 @@ const post_get_all = async (req, res, next) => {
     next(err);
 };
 
+// Get post ID from route parameter and get post from DB. if post exist then send it to the client as JSON
+// else send and error message and code to 'httpError' and return it.
+const post_get = async (req, res, next) => {
+    const postById = await getPost(req.params.postId, next);
+    console.log('get post by id', postById);
+
+    if (postById) {
+        res.json(postById);
+        return;
+    }
+
+    const err = httpError('cat not found', 404);
+    next(err);
+};
+
+// Export functions
 module.exports = {
     post_get_all,
+    post_get,
 };
--- models/postModel.js
+++ models/postModel.js
@@ -4,6 +4,9 @@ const pool = require('../database/db');
 const { httpError } = require('../utils/errors');
 const promisePool = pool.promise();
 
+// Try connecting to DB then execute the SQL query that selects all user_post rows and return them.
+// If there is an error catch it and console log it, also send error message and code to the httpError function
+// to display them to the client
 const getAllPosts = async (next) => {
     try {
         const [rows] = await promisePool.query('SELECT * FROM user_post');
@@ -15,6 +18,24 @@ const getAllPosts = async (next) => {
     }
 };
 
+// Get Post by ID from database
+const getPost = async (postId, next) => {
+    try {
+        const [rows] = await promisePool.execute(
+            `SELECT * FROM user_post WHERE id = ?`,
+            [postId]
+        );
+        console.log('Get post by id', rows);
+        return rows[0];
+    } catch (e) {
+        console.error('error', e.message);
+        const err = httpError('SQL error', 500);
+        next(err);
+    }
+};
+
+// Export functions
 module.exports = {
     getAllPosts,
+    getPost,
 };
