0df51bf7bcaabef1106f85fe373375bee73a99bb
R <63073421+ramizwd@users.noreply.github.com>
2021-12-13 20:14:23 +0200

    changed thumbnail resolution

--- utils/resize.js
+++ utils/resize.js
@@ -4,11 +4,11 @@ const sharp = require('sharp');
 //to make the thumbnail
 const makeThumbnail = async (file, thumbname) => {
     //await for the file
-    return await sharp(file).
-    //resize the size of image
-    resize(160, 160).
-    //where to get the file info
-    toFile('./thumbnails/' + thumbname);
+    return await sharp(file)
+        //resize the size of image
+        .resize(1920, 1080)
+        //where to get the file info
+        .toFile('./thumbnails/' + thumbname);
 };
 module.exports = {
     makeThumbnail,
