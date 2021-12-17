3c13f78cb9bb06c49e347252e164554e161827a3
R <ramz9_97@hotmail.com>
2021-12-17 09:06:38 +0200

    fix

--- models/commentVoteModal.js
+++ models/commentVoteModal.js
@@ -8,7 +8,7 @@ const promisePool = pool.promise();
 const getCommentVote = async (userId, commentId, next) => {
     try {
         const [rows] = await promisePool.execute(
-            'SELECT user_id, vote_type, comments_id FROM pjr_comments_vote WHERE user_id = ? AND comments_id = ?',
+            'SELECT user_id, vote_count, comments_id FROM pjr_comments_vote WHERE user_id = ? AND comments_id = ?',
             [userId, commentId]
         );
         return rows[0];
@@ -23,8 +23,8 @@ const getCommentVote = async (userId, commentId, next) => {
 const commentVoteInsert = async (user, commentVote, commentId, next) => {
     try {
         const [rows] = await promisePool.execute(
-            'INSERT INTO pjr_comments_vote (user_id, vote_type, comments_id) VALUES (?,?,?)',
-            [user.user_id, commentVote.vote_type, commentId]
+            'INSERT INTO pjr_comments_vote (user_id, vote_count, comments_id) VALUES (?,?,?)',
+            [user.user_id, commentVote.vote_count, commentId]
         );
         return rows.insertId;
     } catch (e) {
@@ -38,8 +38,8 @@ const commentVoteInsert = async (user, commentVote, commentId, next) => {
 const deleteCommentVote = async (bodyInfo, commentId, next) => {
     try {
         const [rows] = await promisePool.execute(
-            'DELETE FROM pjr_comments_vote WHERE user_id = ? AND vote_type = ? AND comments_id = ?',
-            [bodyInfo.user_id, bodyInfo.vote_type, commentId]
+            'DELETE FROM pjr_comments_vote WHERE user_id = ? AND vote_count = ? AND comments_id = ?',
+            [bodyInfo.user_id, bodyInfo.vote_count, commentId]
         );
         console.log('vote deleted');
         return rows.affectedRows === 1;
@@ -54,8 +54,8 @@ const deleteCommentVote = async (bodyInfo, commentId, next) => {
 const commentVoteUpdate = async (commentVote, commentId, next) => {
     try {
         const [rows] = await promisePool.execute(
-            'UPDATE pjr_comments_vote SET vote_type = ? WHERE user_id = ? AND comments_id = ?',
-            [commentVote.vote_type, commentVote.user_id, commentId]
+            'UPDATE pjr_comments_vote SET vote_count = ? WHERE user_id = ? AND comments_id = ?',
+            [commentVote.vote_count, commentVote.user_id, commentId]
         );
         console.log('vote updated');
         return rows.affectedRows === 1;
